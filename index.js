e61fa1eba6c3e880edb2e4229b76d799d2cb787d
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 19:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var cart = [];
 
+function genPrice(max) {
+  // generates a number between 1 and max, max inclusive.
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+
 function getCart() {
  return cart;
 }
@@ -11,10 +16,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var newObject = { itemName: item, itemPrice: genPrice(100) };
+ cart.unshift(newObject);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  /* TEMPLATE
+   *  In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+   *  "In your cart, you have "
+   *  `${temp[itemName]} at ${temp[itemPrice]}, `
+   *  `and ${temp[itemName]} at ${temp[itemPrice]}.`
+   */
+
+   var cartOverview = "In your cart, you have ";
+   for (var i = 0; i < cart.length; i++) {
+     var tempItem = cart[i];
+     if (i === cart.length - 1) {
+       cartOverview += `and ${tempItem[itemName]} at ${tempItem[itemPrice]}.`;
+     }
+     else {
+       cartOverview += `${tempItem[itemName]} at ${tempItem[itemPrice]}, `;
+     }
+   }
+
+   return cartOverview;
 }
 
 function total() {
