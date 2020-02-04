4a00490f4477496cfbe9dd3030bcceb4d5a8d584
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 20:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -84,4 +84,10 @@ function placeOrder(cardNumber) {
   if (cardNumber === "") {
     return "Sorry, we don't have a credit card on file for you.";
   }
+
+  cost = total();
+  cart = [];
+
+  return `Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`;
+  
 }
